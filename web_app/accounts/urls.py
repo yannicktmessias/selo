e067cdfe996b6cdd9b85bf7eecfd46d269db4550
Yannick T. Messias <yannicktmessias@gmail.com>
2019-07-06 00:18:40 -0300

    include accounts CRUD functionalities

--- web_app/accounts/urls.py
+++ web_app/accounts/urls.py
@@ -1,7 +1,15 @@
-from django.contrib.auth import views
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('login/', views.LoginView.as_view(template_name = 'accounts/login.html'), name='login'),
-    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('login/', LoginView.as_view(template_name = 'accounts/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('', views.user_info, name='user_info'),
+    path('<int:rf>/', views.user_info, name='user_info'),
+    path('<int:rf>/editar/', views.edit_user, name='edit_user'),
+    path('<int:rf>/excluir/', views.delete_user, name='delete_user'),
+    path('novo/', views.new_user, name='new_user'),
+    path('todos/', views.list_users, name='list_users'),
+    path('procurar/', views.search_user, name='search_user'),
 ]
--- web_app/accounts/views.py
+++ web_app/accounts/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .forms import UserCreationForm, UserChangeForm
+from .models import User
+
+@login_required(login_url='login')
+def user_info(request, rf = None):
+	if rf == None:
+		rf = request.user.rf
+	usr = User.objects.get(rf=rf)
+	return render(request, 'accounts/user_info.html', {'usr': usr})
+
+@login_required(login_url='login')
+def edit_user(request, rf = None):
+	if rf == None:
+		rf = request.user.rf
+	usr = User.objects.get(rf=rf)
+
+	if request.method == 'POST':
+		form = UserChangeForm(request.POST, instance=usr)
+
+		if form.is_valid():
+			form.save()
+			return redirect('user_info', rf=rf)
+		
+		args = {'form': form}
+		return render(request, 'accounts/edit_user.html', args)
+	else:
+		form = UserChangeForm(instance=usr)
+
+		args = {'form': form}
+		return render(request, 'accounts/edit_user.html', args)
+
+@login_required(login_url='login')
+def delete_user(request, rf):
+	User.objects.get(rf=rf).delete()
+	return redirect('list_users')
+
+@login_required(login_url='login')
+def new_user(request):
+	if request.method == 'POST':
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('user_info', rf=request.POST['rf'])
+
+		args = {'form': form}
+		return render(request, 'accounts/new_user.html', args)
+	else:
+		form = UserCreationForm()
+
+		args = {'form': form}
+		return render(request, 'accounts/new_user.html', args)
+
+@login_required(login_url='login')
+def list_users(request):
+	users = User.objects.all()
+	return render(request, 'accounts/list_users.html', {'users': users})
+
+@login_required(login_url='login')
+def search_user(request):
+	pass
